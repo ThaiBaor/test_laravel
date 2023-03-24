6a0400a1585ee940e56a840e9357f5a60c2c9c5c
Student <Student@tdc.edu.vn>
2023-03-24 09:22:24 +0700

    update

--- myapplaravel/routes/web.php
+++ myapplaravel/routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthController;
 
@@ -7,11 +6,6 @@ use App\Http\Controllers\CustomAuthController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
